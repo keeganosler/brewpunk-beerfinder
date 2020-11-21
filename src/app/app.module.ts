1b0fba91c6af99b034e61da72465e29ec350e746
Keegan Osler <keegan.osler@vergeag.com>
2020-11-21 15:39:35 -0700

    search beers method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ViewBeersComponent } from './components/view-beers/view-beers.component';
 import { FilterBeersComponent } from './components/filter-beers/filter-beers.component';
-import { SearchBeersComponent } from './components/filter-beers/search-beers/search-beers.component';
 import { RandomBeerComponent } from './components/filter-beers/random-beer/random-beer.component';
+import { SearchBeersComponent } from './components/filter-beers/search-beers/search-beers.component';
+import { HeaderComponent } from './components/header/header.component';
 import { BeersCarouselComponent } from './components/view-beers/beers-carousel/beers-carousel.component';
 import { BeersListComponent } from './components/view-beers/beers-list/beers-list.component';
-import { SingleBeerComponent } from './components/view-beers/single-beer/single-beer.component';
-import { SingleBeerFrontComponent } from './components/view-beers/single-beer/single-beer-front/single-beer-front.component';
 import { SingleBeerBackComponent } from './components/view-beers/single-beer/single-beer-back/single-beer-back.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTab, MatTabsModule} from '@angular/material/tabs';
+import { SingleBeerFrontComponent } from './components/view-beers/single-beer/single-beer-front/single-beer-front.component';
+import { SingleBeerComponent } from './components/view-beers/single-beer/single-beer.component';
+import { ViewBeersComponent } from './components/view-beers/view-beers.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import {MatTab, MatTabsModule} from '@angular/material/tabs';
     BeersListComponent,
     SingleBeerComponent,
     SingleBeerFrontComponent,
-    SingleBeerBackComponent
+    SingleBeerBackComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,9 +46,10 @@ import {MatTab, MatTabsModule} from '@angular/material/tabs';
     MatDividerModule,
     MatListModule,
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/api-requests.service.ts
+++ src/app/services/api-requests.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment} from '../../environments/environment';
-import { SingleBeerModel} from '../models/single-beer.model';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { SingleBeerModel } from '../models/single-beer.model';
 
 @Injectable()
 export class ApiRequestsService {
   apiUrl = environment.apiUrl;
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
   fetchRandomBeer() {
     return this.http.get<SingleBeerModel[]>(`${this.apiUrl}/beers/random`);
+  }
+
+  searchBeerByString(str: string) {
+    return this.http.get<SingleBeerModel[]>(
+      `${this.apiUrl}/beers?beer_name=${str}`
+    );
   }
 }
