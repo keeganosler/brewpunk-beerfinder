6a7fcdd6a84a442aa6447a354f977c2d2d3e1125
Keegan Osler <keegan.osler@vergeag.com>
2020-12-22 23:00:31 -0700

    beers-list to extend view-beers

--- src/app/components/view-beers/beers-list/beers-list.component.ts
+++ src/app/components/view-beers/beers-list/beers-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ApiRequestsService } from '../../../services/api-requests.service';
+import { StorageService } from '../../../services/storage.service';
+import { ViewBeersComponent } from '../view-beers.component';
 
 @Component({
   selector: 'app-beers-list',
   templateUrl: './beers-list.component.html',
-  styleUrls: ['./beers-list.component.css']
+  styleUrls: ['./beers-list.component.css'],
 })
-export class BeersListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BeersListComponent
+  extends ViewBeersComponent
+  implements OnInit, OnDestroy {
+  constructor(
+    public apiRequestsService: ApiRequestsService,
+    public storageService: StorageService
+  ) {
+    super(apiRequestsService, storageService);
   }
 
+  ngOnInit(): void {
+    super.ngOnInit();
+  }
+
+  ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
 }
