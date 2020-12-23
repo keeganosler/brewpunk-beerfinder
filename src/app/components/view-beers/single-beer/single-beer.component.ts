f2578783f71b0c909deb7dc0fb157e51d85492d9
Keegan Osler <keegan.osler@vergeag.com>
2020-12-22 22:50:53 -0700

    single beer component to extend view beers component

--- src/app/components/view-beers/single-beer/single-beer.component.ts
+++ src/app/components/view-beers/single-beer/single-beer.component.ts
@@ -1,39 +1,49 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SingleBeerModel } from '../../../models/single-beer.model';
+import { ApiRequestsService } from '../../../services/api-requests.service';
 import { StorageService } from '../../../services/storage.service';
+import { ViewBeersComponent } from '../view-beers.component';
 
 @Component({
   selector: 'app-single-beer',
   templateUrl: './single-beer.component.html',
   styleUrls: ['./single-beer.component.css'],
 })
-export class SingleBeerComponent implements OnInit, OnDestroy {
-  constructor(public storageService: StorageService) {
-    this.subs = new Subscription();
+export class SingleBeerComponent
+  extends ViewBeersComponent
+  implements OnInit, OnDestroy {
+  constructor(
+    public apiRequestsService: ApiRequestsService,
+    storageService: StorageService
+  ) {
+    ///this.subs = new Subscription();
+    super(apiRequestsService, storageService);
   }
 
   subs: Subscription;
-  beer: SingleBeerModel;
+  // beer: SingleBeerModel;
 
   singleBeerFront: boolean = true;
 
   ngOnInit(): void {
-    this.subs.add(
-      this.storageService.currentBeer.subscribe((b) => {
-        this.beer = b;
-      })
-    );
+    // this.subs.add(
+    //   this.storageService.currentBeer.subscribe((b) => {
+    //     this.beer = b;
+    //   })
+    // );
+    super.ngOnInit();
   }
 
   ngOnDestroy(): void {
-    if (this.subs) {
-      this.subs.unsubscribe();
-    }
+    // if (this.subs) {
+    //   this.subs.unsubscribe();
+    // }
+    super.ngOnDestroy();
   }
 
   getCurrentBeer(): SingleBeerModel {
-    return this.beer;
+    return this.currentBeer;
   }
 
   switchBackToFront() {
--- src/app/components/view-beers/view-beers.component.ts
+++ src/app/components/view-beers/view-beers.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SingleBeerModel } from '../../models/single-beer.model';
+import { ApiRequestsService } from '../../services/api-requests.service';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-view-beers',
   templateUrl: './view-beers.component.html',
-  styleUrls: ['./view-beers.component.css']
+  styleUrls: ['./view-beers.component.css'],
+  providers: [ApiRequestsService],
 })
-export class ViewBeersComponent implements OnInit {
-
-  constructor() { }
+export class ViewBeersComponent implements OnInit, OnDestroy {
+  subs: Subscription;
+  constructor(
+    public apiRequestsService: ApiRequestsService,
+    public storageService: StorageService
+  ) {
+    this.subs = new Subscription();
+  }
+  beerPool: SingleBeerModel[];
+  currentBeer: SingleBeerModel;
 
   ngOnInit(): void {
+    this.subs.add(
+      this.storageService.beerPool.subscribe((res) => (this.beerPool = res))
+    );
+    this.subs.add(
+      this.storageService.currentBeer.subscribe(
+        (res) => (this.currentBeer = res)
+      )
+    );
   }
 
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
+  }
 }
